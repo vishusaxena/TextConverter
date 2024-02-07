035b6979f192998eabbb779bf45f4376aa3c82e0
vishusaxena <vishusaxena3105@gmail.com>
2024-02-07 10:28:44 +0530

    added new functions

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React , {useState} from 'react'
 export default function Textbox() {
-    return (
-        <>
-            <div className=" container form-group my-4">
-                <h1>Enter text in this Textarea</h1>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="12"></textarea>
-                <button className="btn btn-primary my-3"> Convert in Uppercase</button>
-                <button className="btn btn-primary my-3 mx-4"> Convert in Lowercase</button>
-                <button className="btn btn-primary my-3 mx-3"> Count Words</button>
-                <button className="btn btn-primary my-3 mx-3"> Clear Textarea</button>
+    const [text , setText] = useState("Enter Text here.....")
+    const [show , setBox] = useState(false);
+   
+    const handleupclick=()=>{
+    let newtext=text.toUpperCase();
+     setText(newtext)
 
+    }
+    const handlelowclick=()=>{
+        let newtext=text.toLowerCase();
+         setText(newtext)
+    
+        }
+        const handlecountclick=()=>{
+            setBox(!show);
+    
+        }
+        
+        
+    const handleonchange=(event)=>{
+        setText(event.target.value);
+        
+    }
+    
+    return (
+        
+        <>
+            
+            <div className=" container form-group my-4">
+                <h1>Enter text in the Textarea  </h1>
+                <textarea className="form-control"  value={text} onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                <button className="btn btn-primary my-3" onClick={handleupclick}> Convert in Uppercase</button>
+                <button className="btn btn-primary my-3 mx-4"onClick={handlelowclick}> Convert in Lowercase</button>
+                <button className="btn btn-primary my-3 mx-3" onClick={handlecountclick}> {show?'Hide': 'Give' } Summary</button>
+                
+                 <Box status={show} summ={text} />
                 
 
             </div>
@@ -17,4 +43,14 @@ export default function Textbox() {
 
 
     );
+}
+function Box(props){
+    return(
+        <div className='container my-3' style={{visibility:props.status?'visible':'hidden'}}>
+            <h1>Text Summary</h1>
+            <div className='border border-black py-3 px-3' >{props.summ.length} characters and {props.summ.split(" ").length } words   </div>
+        </div>
+
+    );
+
 }
